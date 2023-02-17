60e6fb7a5bd1b8a674d7c8d1982a8967c9253351
Build Er <builder@macmini6.local>
2023-02-16 23:52:05 -0300

    Package version: 1.0.0-beta+20230217024255

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXObjectsModel",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXObjectsModel",
+			targets: ["GXObjectsModelWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", branch: "beta"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/YAJL.git", branch: "beta")
+	],
+	targets: [
+		.target(name: "GXObjectsModelWrapper",
+				dependencies: [
+					"GXObjectsModel",
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.tvOS, .watchOS, .iOS])),
+					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXObjectsModel",
+			url: "https://pkgs.genexus.dev/iOS/beta/GXObjectsModel-1.0.0-beta+20230217024255.xcframework.zip",
+			checksum: "4f3163b5287829abda3adef3d000a59f3065145de7820417fe8f7fb54318f377"
+		)
+	]
+)
