ffc5469925df92be4fd2a72228fd78753bcd6abe
Build Er <builder@macmini6.local>
2023-05-05 09:45:37 -0300

    Package version: 1.0.0-beta+20230505123704

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
 		.target(name: "GXObjectsModelWrapper",
 				dependencies: [
 					"GXObjectsModel",
-					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS, .tvOS, .watchOS])),
 					.product(name: "YAJL", package: "YAJL", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXObjectsModel",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXObjectsModel-1.0.0-beta+20230504210908.xcframework.zip",
-			checksum: "d92aeadaab379e6d6878d5d06c56153defbcd9a22c1bc9aaaa22120e66037154"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXObjectsModel-1.0.0-beta+20230505123704.xcframework.zip",
+			checksum: "a6abc7adbf93b07d3b1f53287ccfa39350b37b65c6ecd90c7e01f80c972810a4"
 		)
 	]
 )
