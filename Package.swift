09a469ca5cc6b128567e6f9f83ba231d0c6755e4
genexusbot <builder@genexus.com>
2024-01-18 00:36:36 -0300

    Package version: 1.4.0-rc.21

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
 	name: "GXObjectsModel",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
 	products: [
 		.library(
 			name: "GXObjectsModel",
 			targets: ["GXObjectsModelWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.4.0-rc.10")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.4.0-rc.21")
 	],
 	targets: [
 		.target(name: "GXObjectsModelWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXObjectsModel",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXObjectsModel-1.4.0-rc.10.xcframework.zip",
-			checksum: "514ac6fb3a4da84786cc4dfe9f5274661b5da5a2364893aacd60b36d8536b68d"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXObjectsModel-1.4.0-rc.21.xcframework.zip",
+			checksum: "6c7ef6edfb9221f9d7d75f55ea32b9330877bf13ed03976abf048018fdf26c54"
 		)
 	]
 )
