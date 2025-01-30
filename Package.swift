9fdaaaf5ab5c3c5c48d3a42585ffa9da536a9118
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-30 01:09:00 -0300

    Package version: 3.0.0-rc.15

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
 	name: "GXObjectsModel",
-	platforms: [.iOS("13.0"), .watchOS("9.0"), .tvOS("13.0"), .visionOS("1.0")],
+	platforms: [.iOS("15.0"), .watchOS("10.0"), .tvOS("18.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXObjectsModel",
 			targets: ["GXObjectsModelWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "3.0.0-rc.8")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "3.0.0-rc.15")
 	],
 	targets: [
 		.target(name: "GXObjectsModelWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXObjectsModel",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXObjectsModel-3.0.0-rc.8.xcframework.zip",
-			checksum: "1134bc1827600a24383b05e57162669dc9f5b74c9fcb1de98fd34f621487c8e2"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXObjectsModel-3.0.0-rc.15.xcframework.zip",
+			checksum: "fc90e845ca05451ae3a847c17202ac794de77d8aec8079dc1d414d78086adde6"
 		)
 	]
 )
