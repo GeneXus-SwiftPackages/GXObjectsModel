f249558c1e66318788fe32820467412437cc9584
The iOS Team @ GeneXus <builder@genexus.com>
2025-02-25 08:53:15 -0300

    Package version: 3.1.0-beta.3

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXObjectsModelWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "3.1.0-beta.2")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "3.1.0-beta.3")
 	],
 	targets: [
 		.target(name: "GXObjectsModelWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXObjectsModel",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXObjectsModel-3.1.0-beta.2.xcframework.zip",
-			checksum: "73454efdaaec88f9f45249e017b4bed4ecaad99b424ab10f5d09a719f026daa6"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXObjectsModel-3.1.0-beta.3.xcframework.zip",
+			checksum: "81d82e841af6025f9d5bb974bbfc8a63792f59ff91235d0624530c53e51f3e22"
 		)
 	]
 )
